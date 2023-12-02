378d3cb34f9ac9aae4eba61e8b909fc570ba94a4
tssmith7 <tssmith7@users.noreply.github.com>
2023-12-01 20:48:57 -0800

    Added IsConnected func

--- include/AbsoluteEncoder.h
+++ include/AbsoluteEncoder.h
@@ -4,7 +4,7 @@
 #include <units/angle.h>
 
 // Uses the DutyCycleEncoder class to use an SRX mag absolute encoder
-class AbsoluteEncoder{
+class AbsoluteEncoder {
     public:
         // Offset must in the range from 0 to 1
         AbsoluteEncoder( const int absoluteEncoderChannel, const double absoluteEncoderOffset = 0.0) : 
@@ -14,6 +14,10 @@ class AbsoluteEncoder{
         // Returns the angle in degrees
         units::degree_t GetPosition( void ) {
             return ( m_absoluteEncoder.GetAbsolutePosition() - m_absoluteEncoderOffset ) * 360_deg - 180_deg;
+        }
+
+        bool IsConnected( void ) {
+            return m_absoluteEncoder.IsConnected();
         }
     private:
         frc::DutyCycleEncoder m_absoluteEncoder;
